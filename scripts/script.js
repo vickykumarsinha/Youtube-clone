98320b0d3e3ff3203e8b52858c7247b5cde7eeb2
vickykumarsinha <vickykumar898980@gmail.com>
2024-02-24 11:03:39 +0530

    Some Updates

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+function openPlayback(){
+    window.location.href = 'playback.html';
+}
+function openMain(){
+    window.location.href = 'index.html';
+}
+
+function sidebarPop(){
+    var sideBar = document.querySelector('.sideBar');
+    sideBar.classList.toggle(); 
+}
